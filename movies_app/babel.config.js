c86772bff71645a5d52f5ed7d1d616e933ab6f6e
IvanSSantana <ivansantospriv@gmail.com>
2025-10-13 11:36:28 -0300

    Tentando corrigir erro NativeWind.

--- movies_app/babel.config.js
+++ movies_app/babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ["babel-preset-expo"],
-        plugins: [
-            "nativewind/babel", 
-            "react-native-reanimated/plugin"
-        ]
-    };
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+      plugins: [
+          "nativewind/babel", 
+          "react-native-reanimated/plugin"
+      ]
+  };
 }
